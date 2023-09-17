b7b56584a994579694bb5ee88976be725175eeef
Shravan Das <100745475+shravan-das@users.noreply.github.com>
2023-09-17 21:12:54 +0530

    Time: 26 ms (64.92%), Space: 32.5 MB (14.53%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  
+    bool func(ListNode* head, TreeNode* root) {
+      if(head==nullptr){
+        return true;
+      }
+      if(root==nullptr){
+        return false;
+      }
+      
+      return (head->val==root->val) && ((func(head->next,root->left)) or func(head->next,root->right));
+      
+    }
+    bool isSubPath(ListNode* head, TreeNode* root) {
+      if(head==nullptr){
+        return true;
+      }
+      if(root==nullptr){
+        return false;
+      }
+      
+      return head->val==root->val && func(head,root) or isSubPath(head,root->left) or isSubPath(head,root->right);
+    }    
+  
+};
