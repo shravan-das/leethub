7689751edebd3b5f11bd2b2e9287f8cc87aa8820
Shravan Das <100745475+shravan-das@users.noreply.github.com>
2024-01-04 14:57:56 +0530

    Time: 52 ms (58.73%), Space: 42.1 MB (29.99%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+
+ */
+var once = function(fn) {
+    let checker = false;
+    let result;
+    return function(...args){
+      
+      if(!checker){
+        result = fn(...args);
+        checker = true;
+        return result;
+      }
+      else{
+        return undefined;
+      }
+      
+      
+        
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
