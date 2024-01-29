03a89287432c8b6f545d9cf915f7d4130f911ac3
Shravan Das <100745475+shravan-das@users.noreply.github.com>
2024-01-30 00:51:31 +0530

    Time: 1583 ms (76.40%), Space: 0B (100.00%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1280-students-and-examinations/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1280-students-and-examinations.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select Students.student_id , Students.student_name , Subjects.subject_name , count(Examinations.subject_name) as attended_exams from Students CROSS JOIN Subjects LEFT JOIN Examinations on Students.student_id = Examinations.student_id and Subjects.subject_name = Examinations.subject_name group by Students.student_id , Students.student_name, Subjects.subject_name order by student_id , subject_name;
+
+
+
